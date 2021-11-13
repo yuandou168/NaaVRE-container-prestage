b70e7d2f8e47f3a522e487ca45dc96e46e440699
skoulouzis <koulouzis@gmail.com>
2021-11-13 15:41:52 +0100

    build in context

---
+++ build_all.sh
@@ -0,0 +1,14 @@
+counter=0
+for file in ./*/
+do
+  if [ $counter -ge 1 ]; then
+    cd ../
+  fi
+  cd $file
+  wd=`pwd`
+  dockerfile=`ls Dockerfile.*`
+  IFS='.' read -ra names_array <<< "$dockerfile"
+    echo Building ${names_array[1]}/${names_array[2]}
+    docker build . --file $dockerfile --tag ${names_array[1]}/${names_array[2]}
+  ((counter++))
+done
