11d71f533c15b69a05421ccbe6f8d8f2d6d1348e
skoulouzis <skoulouzis@users.noreply.github.com>
2021-11-17 11:55:18 +0100

    chunks-generator creation

---
+++ chunks-generator/chunks-generator.py
@@ -0,0 +1,20 @@
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--text_corpus', action='store', type='', required='True', dest='text_corpus')
+
+arg_parser.add_argument('--param_chunk_size', action='store', type='int', required='True', dest='param_chunk_size')
+
+args = arg_parser.parse_args()
+text_corpus = args.text_corpus
+
+param_chunk_size = args.param_chunk_size
+
+
+
+chunks = [text_corpus[i:i+param_chunk_size] for i in range(0, len(text_corpus), param_chunk_size)]
+
+import json
+outs = {}
+outs['chunks'] = chunks
+print(json.dumps(outs))
