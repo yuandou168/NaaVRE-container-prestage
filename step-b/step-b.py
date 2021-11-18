a03f19d2732191d75f649abcf45f5b68c554bb94
skoulouzis <skoulouzis@users.noreply.github.com>
2021-11-18 14:11:19 +0100

    step-b creation

---
+++ step-b/step-b.py
@@ -0,0 +1,15 @@
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--param_list_size', action='store', type=, required='True', dest='param_list_size')
+
+args = arg_parser.parse_args()
+
+param_list_size = args.param_list_size
+
+
+
+numbers = [el for el in range(0, param_list_size)]
+
+print(numbers)
