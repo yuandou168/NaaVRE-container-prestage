cb0f5417dc9733b3daf36d977a9a64ff476f81ec
skoulouzis <skoulouzis@users.noreply.github.com>
2021-11-23 14:52:39 +0100

    b creation

---
+++ b/b.py
@@ -0,0 +1,13 @@
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--a', action='store', type=, required='True', dest='a')
+
+
+args = arg_parser.parse_args()
+a = args.a
+
+
+
+c = a + 3
+
