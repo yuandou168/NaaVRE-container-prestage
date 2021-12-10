a53b2d5594fe3a77d2b9dae6f4115d01406c4b7d
skoulouzis <skoulouzis@users.noreply.github.com>
2021-12-10 12:13:21 +0100

    test-cell12 creation

---
+++ test-cell12/test-cell12.py
@@ -0,0 +1,14 @@
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--a', action='store', type=, required='True', dest='a')
+
+
+args = arg_parser.parse_args()
+a = args.a
+
+
+
+
+a+=1
+
