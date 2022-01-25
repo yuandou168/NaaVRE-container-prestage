85071818b77af096f56ff205e316605480ddf710
skoulouzis <skoulouzis@users.noreply.github.com>
2022-01-25 17:18:24 +0100

    2-fetch-laz-files creation

---
+++ 2-fetch-laz-files/2-fetch-laz-files.py
@@ -0,0 +1,24 @@
+from laserfarm.remote_utils import list_remote
+from laserfarm.remote_utils import get_wdclient
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--param_webdav_login', action='store', type=str, required='True', dest='param_webdav_login')
+arg_parser.add_argument('--param_webdav_password', action='store', type=str, required='True', dest='param_webdav_password')
+
+args = arg_parser.parse_args()
+
+param_webdav_login = args.param_webdav_login
+param_webdav_password = args.param_webdav_password
+
+conf_wd_opts = {'webdav_hostname': 'https://webdav.grid.surfsara.nl:2880', 'webdav_login': param_webdav_login, 'webdav_password': param_webdav_password}
+conf_remote_path_ahn = pathlib.Path('/pnfs/grid.sara.nl/data/projects.nl/eecolidar/02_UvA/YShi/Spain/Spain_test')
+
+conf_wd_opts = {'webdav_hostname': 'https://webdav.grid.surfsara.nl:2880', 'webdav_login': param_webdav_login, 'webdav_password': param_webdav_password}
+conf_remote_path_ahn = pathlib.Path('/pnfs/grid.sara.nl/data/projects.nl/eecolidar/02_UvA/YShi/Spain/Spain_test')
+
+laz_files = [f for f in list_remote(get_wdclient(conf_wd_opts), conf_remote_path_ahn.as_posix())
+             if f.lower().endswith('.laz')]
+
+print(laz_files)
