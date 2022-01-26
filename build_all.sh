7ca7a649d95ca8bed9ab4224bc543e268a5426d3
skoulouzis <koulouzis@gmail.com>
2022-01-26 16:52:04 +0100

    build only changed folders

--- build_all.sh
+++ build_all.sh
@@ -1,17 +1,19 @@
-VAR=`git diff --name-only HEAD HEAD~1`
+PARRENT_COMMIT=`git rev-list --all --parents --max-count=1`
+IFS=' ' read -ra COMMIT <<< "$PARRENT_COMMIT"
+VAR=`git diff --name-only HEAD ${COMMIT[1]}`
 DIR="$(dirname "${VAR}")" ; FILE="$(basename "${VAR}")"
 if [ "$DIR" != "." ]; then
   cd $DIR
   wd=`pwd`
   dockerfile=`ls Dockerfile.*`
   IFS='.' read -ra names_array <<< "$dockerfile"
-   echo Building ${names_array[1]}/${names_array[2]}
-   if [ $1 = "build" ]; then
-     docker build . --file $dockerfile --tag ${names_array[1]}/${names_array[2]}
-   fi
-   if [ $1 = "push" ]; then
-     docker push ${names_array[1]}/${names_array[2]}
-   fi
+  echo Building ${names_array[1]}/${names_array[2]}
+  if [ $1 = "build" ]; then
+   docker build . --file $dockerfile --tag ${names_array[1]}/${names_array[2]}
+  fi
+  if [ $1 = "push" ]; then
+   docker push ${names_array[1]}/${names_array[2]}
+  fi
 fi
 
 
