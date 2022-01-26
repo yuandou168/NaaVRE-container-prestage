196e89f9f19a7d2978e0753e8eb0504631c5cad3
skoulouzis <skoulouzis@users.noreply.github.com>
2022-01-26 16:24:42 +0100

    Update build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -1,11 +1,6 @@
-counter=0
-for file in ./*/
-do
-  if [ $counter -ge 1 ]; then
-    cd ../
-  fi
-  cd $file
-  wd=`pwd`
+VAR=`git diff --name-only HEAD HEAD~1`
+DIR="$(dirname "${VAR}")" ; FILE="$(basename "${VAR}")"
+cd $DIR
   dockerfile=`ls Dockerfile.*`
   IFS='.' read -ra names_array <<< "$dockerfile"
     echo Building ${names_array[1]}/${names_array[2]}
@@ -15,5 +10,23 @@ do
     if [ $1 = "push" ]; then
       docker push ${names_array[1]}/${names_array[2]}
     fi
-  ((counter++))
-done
+    
+# counter=0
+# for file in ./*/
+# do
+#   if [ $counter -ge 1 ]; then
+#     cd ../
+#   fi
+#   cd $file
+#   wd=`pwd`
+#   dockerfile=`ls Dockerfile.*`
+#   IFS='.' read -ra names_array <<< "$dockerfile"
+#     echo Building ${names_array[1]}/${names_array[2]}
+#     if [ $1 = "build" ]; then
+#       docker build . --file $dockerfile --tag ${names_array[1]}/${names_array[2]}
+#     fi
+#     if [ $1 = "push" ]; then
+#       docker push ${names_array[1]}/${names_array[2]}
+#     fi
+#   ((counter++))
+# done
