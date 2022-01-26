d7ffffe36c5897a37992d5e6d4fbfe639d5cf277
skoulouzis <skoulouzis@users.noreply.github.com>
2022-01-26 17:16:01 +0100

    Update 3.-retiling.py

--- 3.-retiling/3.-retiling.py
+++ 3.-retiling/3.-retiling.py
@@ -1,7 +1,6 @@
 from laserfarm import Retiler
 import argparse
 arg_parser = argparse.ArgumentParser()
-
 arg_parser.add_argument('--laz_files', action='store', type=list, required='True', dest='laz_files')
 
 arg_parser.add_argument('--param_webdav_login', action='store', type=str, required='True', dest='param_webdav_login')
