212517bf31992066f3b0d064decb2b6efceb3768
skoulouzis <koulouzis@gmail.com>
2022-01-27 13:42:38 +0100

    trigger build

--- configurations-1/configurations-1.py
+++ configurations-1/configurations-1.py
@@ -5,5 +5,6 @@ conf_remote_path_ahn = pathlib.Path('/webdav/ahn')
 conf_remote_path_retiled = pathlib.Path('/webdav/retiled/')
 conf_remote_path_targets = pathlib.Path('/webdav/targets')
 conf_local_tmp = pathlib.Path('/tmp')
+
 conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
 
