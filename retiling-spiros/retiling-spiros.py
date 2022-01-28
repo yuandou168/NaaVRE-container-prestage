7c5a2692a78a18709fd5a3e0f23d348118ef90aa
skoulouzis <skoulouzis@users.noreply.github.com>
2022-01-28 18:50:56 +0100

    Update retiling-spiros.py

--- retiling-spiros/retiling-spiros.py
+++ retiling-spiros/retiling-spiros.py
@@ -2,7 +2,7 @@ from laserfarm import Retiler
 import argparse
 arg_parser = argparse.ArgumentParser()
 
-arg_parser.add_argument('--laz_files', action='store', type=list, required='True', dest='laz_files')
+arg_parser.add_argument('--laz_files', action='store', type=str, required='True', dest='laz_files')
 
 arg_parser.add_argument('--param_hostname', action='store', type=str, required='True', dest='param_hostname')
 arg_parser.add_argument('--param_login', action='store', type=str, required='True', dest='param_login')
