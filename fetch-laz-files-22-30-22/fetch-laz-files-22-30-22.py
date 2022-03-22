5b66acfead53ebce237f10ada6ba38de6c1491bb
skoulouzis <skoulouzis@users.noreply.github.com>
2022-03-22 23:58:42 +0100

    fetch-laz-files-22-30-22 creation

---
+++ fetch-laz-files-22-30-22/fetch-laz-files-22-30-22.py
@@ -0,0 +1,28 @@
+from laserfarm.remote_utils import list_remote
+from laserfarm.remote_utils import get_wdclient
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--param_hostname', action='store', type=, required='True', dest='param_hostname')
+arg_parser.add_argument('--param_login', action='store', type=, required='True', dest='param_login')
+arg_parser.add_argument('--param_password', action='store', type=, required='True', dest='param_password')
+
+args = arg_parser.parse_args()
+
+param_hostname = args.param_hostname
+param_login = args.param_login
+param_password = args.param_password
+
+conf_remote_path_ahn = pathlib.Path('/webdav/ahn')
+conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+
+conf_remote_path_ahn = pathlib.Path('/webdav/ahn')
+conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+laz_files = [f for f in list_remote(get_wdclient(conf_wd_opts), conf_remote_path_ahn.as_posix())
+             if f.lower().endswith('.laz')]
+
+import json
+outs = {}
+outs['laz_files'] = laz_files
+print(json.dumps(outs))
