509a386914ba830d738974987e054c53be695e3a
skoulouzis <skoulouzis@users.noreply.github.com>
2022-03-22 22:17:25 +0100

    retiling-22-30-22 creation

---
+++ retiling-22-30-22/retiling-22-30-22.py
@@ -0,0 +1,53 @@
+from laserfarm import Retiler
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--laz_files', action='store' , type= , required='True', dest='laz_files')
+
+arg_parser.add_argument('--param_hostname', action='store', type=, required='True', dest='param_hostname')
+arg_parser.add_argument('--param_login', action='store', type=, required='True', dest='param_login')
+arg_parser.add_argument('--param_password', action='store', type=, required='True', dest='param_password')
+
+args = arg_parser.parse_args()
+laz_files = args.laz_files
+
+param_hostname = args.param_hostname
+param_login = args.param_login
+param_password = args.param_password
+
+conf_remote_path_ahn = pathlib.Path('/webdav/ahn')
+conf_remote_path_retiled = pathlib.Path('/webdav/retiled/')
+conf_local_tmp = pathlib.Path('/tmp')
+conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+
+conf_remote_path_ahn = pathlib.Path('/webdav/ahn')
+conf_remote_path_retiled = pathlib.Path('/webdav/retiled/')
+conf_local_tmp = pathlib.Path('/tmp')
+conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+
+remote_path_retiled = conf_remote_path_retiled
+
+grid_retile = {
+    'min_x': -113107.81,
+    'max_x': 398892.19,
+    'min_y': 214783.87,
+    'max_y': 726783.87,
+    'n_tiles_side': 512
+}
+
+
+retiling_input = {
+    'setup_local_fs': {'tmp_folder': conf_local_tmp.as_posix()},
+    'pullremote': conf_remote_path_ahn.as_posix(),
+    'set_grid': grid_retile,
+    'split_and_redistribute': {},
+    'validate': {},
+    'pushremote': conf_remote_path_retiled.as_posix(),
+    'cleanlocalfs': {}
+}
+
+file = laz_files
+retiler = Retiler(file).config(retiling_input).setup_webdav_client(conf_wd_opts)
+retiler.run()
+
+print(remote_path_retiled)
