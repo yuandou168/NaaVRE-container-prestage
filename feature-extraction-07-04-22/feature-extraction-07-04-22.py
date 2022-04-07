a6c4dbfce8a6cc738606c9dc91447759d12496db
skoulouzis <skoulouzis@users.noreply.github.com>
2022-04-07 16:55:37 +0200

    feature-extraction-07-04-22 creation

---
+++ feature-extraction-07-04-22/feature-extraction-07-04-22.py
@@ -0,0 +1,104 @@
+from laserfarm import DataProcessing
+import pathlib
+import pathlib
+import pathlib
+import argparse
+import json
+from json import JSONDecodeError
+arg_parser = argparse.ArgumentParser()
+from os.path import exists
+inputs_path = '/tmp/inputs.json'
+inputs_exists = exists(inputs_path)
+
+if inputs_exists:
+    with open(inputs_path) as json_file:
+        try:
+            inputs = json.load(json_file)
+        except JSONDecodeError:
+            json_file.close()
+            f = open(inputs_path, "r")
+            inputs = f.readlines()[0].strip()
+
+if inputs:
+    if 'tiles' in inputs:
+        tiles = inputs['tiles']
+    else:
+        tiles = inputs
+else:
+    arg_parser.add_argument('--tiles', action='store' , required='True', dest='tiles')
+
+arg_parser.add_argument('--param_hostname', action='store', type=str, required='True', dest='param_hostname')
+arg_parser.add_argument('--param_login', action='store', type=str, required='True', dest='param_login')
+arg_parser.add_argument('--param_password', action='store', type=str, required='True', dest='param_password')
+arg_parser.add_argument('--param_validate_precision', action='store', type=float, required='True', dest='param_validate_precision')
+
+args = arg_parser.parse_args()
+if not inputs_exists:
+    tiles = args.tiles
+
+param_hostname = args.param_hostname
+param_login = args.param_login
+param_password = args.param_password
+param_validate_precision = args.param_validate_precision
+
+conf_local_tmp = pathlib.Path('/tmp')
+conf_remote_path_targets = pathlib.Path('/webdav/targets')
+conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+conf_remote_path_retiled = pathlib.Path('/webdav/retiled/')
+
+conf_local_tmp = pathlib.Path('/tmp')
+conf_remote_path_targets = pathlib.Path('/webdav/targets')
+conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+conf_remote_path_retiled = pathlib.Path('/webdav/retiled/')
+
+t = tiles
+
+features = ['perc_95_normalized_height']
+
+tile_mesh_size = 10.
+
+
+
+grid_feature = {
+    'min_x': -113107.81,
+    'max_x': 398892.19,
+    'min_y': 214783.87,
+    'max_y': 726783.87,
+    'n_tiles_side': 512
+}
+
+feature_extraction_input = {
+    'setup_local_fs': {'tmp_folder': conf_local_tmp.as_posix()},
+    'pullremote': conf_remote_path_retiled.as_posix(),
+    'load': {'attributes': ['raw_classification']},
+    'normalize': 1,
+    'apply_filter': {
+        'filter_type': 'select_equal', 
+        'attribute': 'raw_classification',
+        'value': [1, 6]#ground surface (2), water (9), buildings (6), artificial objects (26), vegetation (?), and unclassified (1)
+    },
+    'generate_targets': {
+        'tile_mesh_size' : tile_mesh_size,
+        'validate' : True,
+        'validate_precision': param_validate_precision
+        **grid_feature
+    },
+    'extract_features': {
+        'feature_names': features,
+        'volume_type': 'cell',
+        'volume_size': tile_mesh_size
+    },
+    'export_targets': {
+        'attributes': features,
+        'multi_band_files': False
+    },
+    'pushremote': conf_remote_path_targets.as_posix(),
+}
+idx = (t.split('_')[1:])
+processing = DataProcessing(t, tile_index=idx).config(feature_extraction_input).setup_webdav_client(conf_wd_opts)
+processing.run()
+
+import json
+with open('/tmp/outputs.json', 'w') as f:
+    json.dump(features, f)
+
