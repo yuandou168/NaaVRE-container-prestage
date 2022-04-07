5b1f4379e8148fe860c3e62858527b60fd9f01ea
Riccardo Bianchi <contervis@gmail.com>
2022-04-07 20:50:30 +0200

    word-search-v1 creation

---
+++ word-search-v1/word-search-v1.py
@@ -0,0 +1,19 @@
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--chunks', action='store' , type=str , required='True', dest='chunks')
+
+arg_parser.add_argument('--param_word', action='store', type=str, required='True', dest='param_word')
+
+args = arg_parser.parse_args()
+chunks = args.chunks
+
+param_word = args.param_word
+
+
+
+found = param_word in chunks.split()
+
+file_found = open("/tmp/found.json", "w")
+file_found.write(json.dumps(found))
+file_found.close()
