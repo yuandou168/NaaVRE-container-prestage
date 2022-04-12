30e5588dc6b2c17e97d593428e663b8e0ed0021f
Riccardo Bianchi <contervis@gmail.com>
2022-04-12 14:03:07 +0200

    generate-word-search-chunks-v4 creation

---
+++ generate-word-search-chunks-v4/generate-word-search-chunks-v4.py
@@ -0,0 +1,26 @@
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--param_chunk_size', action='store', type=int, required='True', dest='param_chunk_size')
+arg_parser.add_argument('--param_text_corpus', action='store', type=str, required='True', dest='param_text_corpus')
+
+args = arg_parser.parse_args()
+
+id = args.id
+
+
+param_chunk_size = args.param_chunk_size
+param_text_corpus = args.param_text_corpus
+
+
+
+chunks = [param_text_corpus[i:i+param_chunk_size] for i in range(0, len(param_text_corpus), param_chunk_size)]
+
+import json
+filename = "/tmp/chunks_" + id + ".json"
+file_chunks = open(filename, "w")
+file_chunks.write(json.dumps(chunks))
+file_chunks.close()
